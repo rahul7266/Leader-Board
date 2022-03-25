b9a443a5a28d8e2aaba33a58d8f1ce140bfba11b
rahul7266 <singhrahul4620011@gmail.com>
2022-03-25 22:11:06 +0530

    leader

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import CreateComponent from './create';
+import Navbar from './Navbar';
+import Home from './Home';
+import { BrowserRouter as Router,Route,Switch} from 'react-router-dom'
+import {useState} from "react";
+
 
 function App() {
+  
+  const [members,setMembers] = useState([
+    {
+        name:'xyz',
+        score: 123,
+        img:''
+    }
+]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <Router>
+         <div className="App">
+           <Navbar/>
+           <div className="content">
+              <Switch>
+                  <Route exact path="/">
+                     <Home
+                     members={members}
+                     setMembers={setMembers}
+                     />
+                  </Route>
+                  <Route path="/create">
+                     <CreateComponent
+                                          members={members}
+                                          setMembers={setMembers}
+                     />
+                  </Route>
+              </Switch>
+           </div>
+      
+         </div>
+     </Router>
   );
 }
 
